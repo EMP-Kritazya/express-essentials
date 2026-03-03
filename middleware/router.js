a1c1b636e2e1357d7e1d65b9024d13acf327552a
EMP-Kritazya <emp.kritazya@gmail.com>
2026-03-02 22:14:34 -0600

    used routes to process routing. Understood they basically help routing specific requests (real world use case)

--- middleware/router.js
+++ middleware/router.js
@@ -6,6 +6,42 @@ const app = express();
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log("Time:", new Date(Date.now()).toString());
+  next();
+});
+
+router.use(
+  "/user/:id",
+  (req, res, next) => {
+    console.log("Requested URL: ", req.originalUrl);
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request Type: ", req.method);
+    next();
+  },
+);
+
+router.get(
+  "/user/:id",
+  (req, res, next) => {
+    if (req.params.id === "0") {
+      next("route");
+    } else next();
+  },
+  (req, res, next) => {
+    res.send("Hello Average User");
+  },
+);
+
+router.get("/user/:id", (req, res, next) => {
+  console.log(req.params.id);
+  res.send("Hello Admin");
+});
+
+app.use("/", router);
+
 app.listen(port, () => {
   console.log(`Server Started at: http://localhost:${port}`);
 });
